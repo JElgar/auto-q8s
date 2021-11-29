72ba0f78c7d36aaa2102bfde4d353359a084e5aa
James Elgar <jamesnelgar@gmail.com>
2021-11-29 16:47:19 +0000

    Install cert manager

--- k8s/init.sh
+++ k8s/init.sh
@@ -1,7 +1,11 @@
+# Install calico pod network
 kubectl create -f https://docs.projectcalico.org/manifests/tigera-operator.yaml
 kubectl create -f https://docs.projectcalico.org/manifests/custom-resources.yaml
+
+# Untain nodes (allow container to run on them)
 kubectl taint nodes --all node-role.kubernetes.io/master-
 
+# Install istio
 istioctl operator init
 kubectl create ns istio-system
 kubectl apply -f - <<EOF
@@ -14,8 +18,12 @@ spec:
   profile: default
 EOF
 
+# Install metallb
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-
+## Apply metallb config (created in glue script)
 kubectl apply -f metallb_configmap.yml
+
+# Install cert manager
+kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.6.1/cert-manager.yaml
