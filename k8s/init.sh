74a235820c5c0b930fb90417b1a0a03d8b9dc5bc
jelgar <jamesnelgar@gmail.com>
2021-12-01 14:49:06 +0000

    Add longhorn dependencies

--- k8s/init.sh
+++ k8s/init.sh
@@ -29,3 +29,6 @@ envsubst < metallb/metallb_configmap.yml | kubectl apply -f -
 # Install cert manager
 kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.6.1/cert-manager.yaml
 kubectl apply -f cert-manager
+
+# Install longhorn
+kubectl apply -f https://raw.githubusercontent.com/longhorn/longhorn/v1.2.2/deploy/longhorn.yaml
