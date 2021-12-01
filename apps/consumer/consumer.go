98c3c721319e1b4daa1eb00dd8221023d6281212
James Elgar <jamesnelgar@gmail.com>
2021-12-01 16:08:46 +0000

    Added idler

--- apps/consumer/consumer.go
+++ apps/consumer/consumer.go
@@ -2,12 +2,35 @@ package main
 
 import (
 	"log"
+    "runtime"
+    "time"
     "apps/services"
 )
+
+func idle() {
+    done := make(chan int)
+    
+    for i := 0; i < runtime.NumCPU(); i++ {
+        go func() {
+            for {
+                select {
+                case <-done:
+                    return
+                default:
+                }
+            }
+        }()
+    }
+    
+    time.Sleep(time.Second * 10)
+    close(done)
+}
 
 func main() {
     rmq := services.RabbitmqSetup()
     for d := range rmq.Consumer() {
         log.Printf("Doing work on: %s", d.Body)
+        idle()
+        log.Printf("Done work on: %s", d.Body)
     }
 }
